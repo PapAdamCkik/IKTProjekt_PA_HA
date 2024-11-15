5db7930519591b0585b40bf304eb32a1c70ad7d8
NagyKristofckik <nagy.kristof2@ckik.hu>
2024-11-15 20:01:38 +0100

    menukesz

--- papteamwork.py
+++ papteamwork.py
@@ -1,47 +1,42 @@
 import time
 import os
+menu = 0
+while menu!=5:
+# tomb = [0, 0, 0, 0, 0, 0, 0, 0, 0]
 
-tomb = [0, 0, 0, 0, 0, 0, 0, 0, 0]
-
-print("1) 3a feladat")
-print("2) 3b feladat")
-print("3) 3c feladat")
-print("4) 3d feladat")
-print("5) Kilépés")
-menu = int(input())
-os.system("clear")
-if menu == 1:
-    print("3a feladat")
-    print("1) kód inditása")
-    print("2) értékek modositás!")
-    menu = int(input())
-elif menu == 2:
-    print("3b feladat")
-    print("1) kód inditása")
-    print("2) értékek modositás!")
-    menu = int(input())
-elif menu == 3:
-    print("3c feladat")
-    print("1) kód inditása")
-    print("2) értékek modositás!")
-    menu = int(input())
-elif menu == 4:
-    print("3d feladat")
-    print("1) kód inditása")
-    print("2) értékek modositás!")
-    menu = int(input())
-elif menu == 5:
-    print("viszlát!")
-    time.sleep(3)
-    exit
-else:
-    print("Jó számot adj meg!")
+    if menu== 0:
+        print("1) 3a feladat")
+        print("2) 3b feladat")
+        print("3) 3c feladat")
+        print("4) 3d feladat")
+        print("5) Kilépés")
+        menu = int(input())
     
-if menu== 2:
-    for i in range(len(tomb)):
-        ujszam = input()
-        tomb[i] = ujszam
+    if menu == 1:
+        print("3a feladat")
+        menu = 0
+    elif menu == 2:
+        print("3b feladat")
+        menu = 0
+    elif menu == 3:
+        print("3c feladat")
+        menu = 0
+    elif menu == 4:
+        print("3d feladat")
+        menu = 0
+    elif menu == 5:
+        print('')
+    else:
         
-    print(tomb)
-#else:
-   # itt fog futni a kód
+        print("Jó számot adj meg!")
+        
+
+    # for i in range(len(tomb)):
+    #     ujszam = input()
+    #     tomb[i] = ujszam
+            
+        # print(tomb)
+    #else:
+    # itt fog futni a kód    
+print("viszlát!")
+time.sleep(3)   
